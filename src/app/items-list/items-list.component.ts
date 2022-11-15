08edd13d1e4a467bf472eaaf8e979aba4733f720
CORP\Pat.Junio <pat.junio@softwarefrontier.com.ph>
2022-11-15 16:06:25 +0800

    - Added display emptyList component if list is empty and tableList if shoppingList has item.

--- src/app/items-list/items-list.component.ts
+++ src/app/items-list/items-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { LocalDataService } from '../services/local-data.service';
 import { Items } from '../shoppingList';
 
@@ -8,44 +8,51 @@ import { Items } from '../shoppingList';
   templateUrl: './items-list.component.html',
   styleUrls: ['./items-list.component.scss']
 })
-export class ItemsListComponent implements OnInit {
-   ngOnInit(): void {
-  throw new Error('Method not implemented.');
-}
 
-constructor(public localDataService: LocalDataService) {
-}
+export class ItemsListComponent {
 
-model = new Items(1, 'MILK', 'This is an evaporated milk.', 7, 143.25);
+  constructor(public localDataService: LocalDataService) {
 
-submitted = false;
+  }
 
-onSubmit() {
-  this.localDataService.shoppingItems.push(
-    new Items(
-      this.model.itemID++,
-      this.model.itemName,
-      this.model.itemDescription,
-      this.model.itemQuantity,
-      this.model.itemPrice
-    ));
+  itemList = true;
 
+  model = new Items(1, 'MILK', 'This is an evaporated milk.', 7, 143.25);
+
+  submitted = false;
+
+  onSubmit() {
+    this.localDataService.shoppingItems.push(
+      new Items(
+        this.model.itemID++,
+        this.model.itemName,
+        this.model.itemDescription,
+        this.model.itemQuantity,
+        this.model.itemPrice
+      ));
+    console.log(Items);
     this.submitted = true;
-}
+  }
 
-newItem(): void {
-  this.model = new Items(12, 'MILO', 'This is a energy drink.', 8, 143.75);
-}
+  newItem(): void {
+    this.model = new Items(12, 'MILO', 'This is a energy drink.', 8, 143.75);
+  }
 
-showFormControls(list: any) {
-  return list && list.controls.itemName &&
-    list.controls.itemName.value;
-}
+  showFormControls(list: any) {
+    return list && list.controls.itemName &&
+      list.controls.itemName.value;
+  }
 
-onClick(): void {
-  
-}
+  onClick(): void {
 
-title = 'Add Items';
+  }
+
+  clearItem() {
+    this.localDataService.shoppingItems = [];
+    return this.newItem;
+    console.log('clearItem')
+  }
+
+  title = 'Add Items';
 }
 
